7444959e6a924e062177ba700df69f2c343ba886
Sviut <44402566+Sviut@users.noreply.github.com>
2020-09-06 16:40:43 +0300

    add exstract data and return after request

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -33,7 +33,15 @@ class APIService {
       headers: { "Authorization": "Bearer $accessToken" }
     );
     if (response.statusCode == 200) {
-
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint];
+        final int result = endpointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
     }
   }
 
