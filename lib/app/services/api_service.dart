ced39c4a45e68c1becffdf4bc5b832bedf707367
Sviut <44402566+Sviut@users.noreply.github.com>
2020-09-06 21:01:08 +0300

    add endpointData & change response

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:corona_flutter/app/services/endpoint_data.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:corona_flutter/app/services/api.dart';
@@ -8,10 +9,10 @@ class APIService {
   APIService(this.api);
   final API api;
 
-  Future<String> getAccessToken() async{
+  Future<String> getAccessToken() async {
     final response = await http.post(
       api.tokenUri().toString(),
-      headers: {'Authorization':'Basic ${api.apiKey}'},
+      headers: {'Authorization': 'Basic ${api.apiKey}'},
     );
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
@@ -23,23 +24,23 @@ class APIService {
     throw response;
   }
 
-  Future<int> getEndpointData({
+  Future<EndpointData> getEndpointData({
     @required String accessToken,
     @required Endpoint endpoint,
   }) async {
     final uri = api.endpointUri(endpoint);
-    final response = await http.get(
-      uri.toString(),
-      headers: { "Authorization": "Bearer $accessToken" }
-    );
+    final response = await http
+        .get(uri.toString(), headers: {"Authorization": "Bearer $accessToken"});
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
       if (data.isNotEmpty) {
         final Map<String, dynamic> endpointData = data[0];
         final String responseJsonKey = _responseJsonKeys[endpoint];
-        final int result = endpointData[responseJsonKey];
-        if (result != null) {
-          return result;
+        final int value = endpointData[responseJsonKey];
+        final String dateString = endpointData['date'];
+        final date = DateTime.tryParse(dateString);
+        if (value != null) {
+          return EndpointData(value: value, date: date);
         }
       }
     }
