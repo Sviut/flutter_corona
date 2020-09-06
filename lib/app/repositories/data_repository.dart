199245549ed0a7ee9b99781a96dab848af1b5ed7
Sviut <44402566+Sviut@users.noreply.github.com>
2020-09-06 20:06:56 +0300

    mapping data for types

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,3 +1,4 @@
+import 'package:corona_flutter/app/repositories/endpoints_data.dart';
 import 'package:corona_flutter/app/services/api.dart';
 import 'package:corona_flutter/app/services/api_service.dart';
 import 'package:flutter/foundation.dart';
@@ -27,8 +28,8 @@ class DataRepository {
     }
   }
 
-  Future<void> _getAllEndpointsData() async {
-    await Future.wait([
+  Future<EndpointsData> _getAllEndpointsData() async {
+    final values = await Future.wait([
       apiService.getEndpointData(
           accessToken: _accessToken, endpoint: Endpoint.cases),
       apiService.getEndpointData(
@@ -40,5 +41,13 @@ class DataRepository {
       apiService.getEndpointData(
           accessToken: _accessToken, endpoint: Endpoint.recovered),
     ]);
+
+    return EndpointsData(values: {
+      Endpoint.cases: values[0],
+      Endpoint.casesSuspected: values[1],
+      Endpoint.casesConfirmed: values[2],
+      Endpoint.deaths: values[3],
+      Endpoint.recovered: values[4],
+    });
   }
 }
