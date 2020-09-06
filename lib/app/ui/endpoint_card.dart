606bc542af88381aaf124ed00ef3454a35753e4d
Sviut <44402566+Sviut@users.noreply.github.com>
2020-09-06 19:33:09 +0300

    add endpoint card component

--- lib/app/ui/endpoint_card.dart
+++ lib/app/ui/endpoint_card.dart
@@ -1,8 +1,27 @@
+import 'package:corona_flutter/app/services/api.dart';
 import 'package:flutter/material.dart';
 
 class EndpointCard extends StatelessWidget {
+  const EndpointCard({Key key, this.endpoint, this.value}) : super(key: key);
+
+  final Endpoint endpoint;
+  final int value;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: Row(
+        children: <Widget>[
+          Text(
+            'Cases',
+            style: Theme.of(context).textTheme.headline2,
+          ),
+          Text(
+            value != null ? value.toString() : '',
+            style: Theme.of(context).textTheme.headline2,
+          )
+        ],
+      ),
+    );
   }
 }
