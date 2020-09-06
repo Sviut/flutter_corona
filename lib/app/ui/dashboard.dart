746a2bb953e6d475131a9d3562ead30ac1adfef1
Sviut <44402566+Sviut@users.noreply.github.com>
2020-09-06 20:20:50 +0300

    add iteration response for list

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:corona_flutter/app/repositories/data_repository.dart';
+import 'package:corona_flutter/app/repositories/endpoints_data.dart';
 import 'package:corona_flutter/app/services/api.dart';
 import 'package:corona_flutter/app/ui/endpoint_card.dart';
 import 'package:flutter/cupertino.dart';
@@ -11,7 +12,7 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  int _cases;
+  EndpointsData _endpointsData;
 
   @override
   void initState() {
@@ -21,8 +22,8 @@ class _DashboardState extends State<Dashboard> {
 
   Future<void> _updateData() async {
     final dataRepository = Provider.of<DataRepository>(context, listen: false);
-    final cases = await dataRepository.getEndpointData(Endpoint.cases);
-    setState(() => _cases = cases);
+    final endpointsData = await dataRepository.getAllEndpointsData();
+    setState(() => _endpointsData = endpointsData);
   }
 
   @override
@@ -35,10 +36,13 @@ class _DashboardState extends State<Dashboard> {
         onRefresh: _updateData,
         child: ListView(
           children: <Widget>[
-            EndpointCard(
-              endpoint: Endpoint.cases,
-              value: _cases,
-            ),
+            for (var endpoint in Endpoint.values)
+              EndpointCard(
+                endpoint: endpoint,
+                value: _endpointsData != null
+                    ? _endpointsData.values[endpoint]
+                    : null,
+              ),
           ],
         ),
       ),
